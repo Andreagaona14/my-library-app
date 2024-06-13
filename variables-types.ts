9c1219d9e48d43b7dc3fca9202cc63d9b961567c
unknown <andreitagaona05@gmail.com>
2024-06-13 07:10:08 -0500

    tipado primitivo

---
+++ variables-types.ts
@@ -0,0 +1,24 @@
+let bookTitle : string ="TypeScript Basics";
+
+const libraryName : string = "My Library"; 
+
+let numberOfBooks : number = 10;
+
+let isLibraryOpen : boolean = true; 
+
+let titulo : string;
+let autor  : string;
+let genero : string;
+let idioma : string;
+let precio : number;
+let formato : string;
+let isbn : number;
+let descripcion : string;
+let estado : string;
+let ubicacion : string;
+let fecha_publcacion : Date;
+let editorial : string;
+let paginas : number;
+let dimesiones : number;
+let peso : string;
+let disponible : string;
